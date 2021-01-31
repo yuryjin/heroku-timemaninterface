de21c4d5810ad5d6a6b2cc67a97ccaab556f0223
aquinas <aquinas1776@gmail.com>
2021-02-01 00:09:42 +0300

    test new

--- src/components/Components.vue
+++ src/components/Components.vue
@@ -38,6 +38,7 @@
               <td>
                 <button class="brn btn-success btn-sm mx-1" @click="$data.table = table">Edit</button>
                 <button class="brn btn-danger btn-sm mx-1" @click="deleteTable(table)">Delete</button>
+                <button class="brn btn-danger btn-sm mx-1" @click="deleteTableAxios(table)">Delete Axios</button>
               </td>
             </tr>
           </tbody>
@@ -52,6 +53,8 @@
 </style>
 
 <script>
+  import axios from 'axios';
+
   export default {
     name: 'Components',
     data() {
@@ -62,27 +65,27 @@
       }
     },
     async created() {
-      await this.getTables();
+      await this.getTablesAxios();
     },
 
     methods: {
       submitForm() {
         if (this.table.id === undefined) {
-          this.createTable();
+          this.createTableAxios();
         } else {
-          this.editTable();
+          this.editTableAxios();
         }
       },
 
 
       async getTables() {
-        var response = await fetch('https://heroku-timeman.herokuapp.com/database/tables/');
+        var response = await fetch('http://localhost:8000/database/tables/');
         this.tables = await response.json();
       },
       async createTable() {
         await this.getTables();
 
-        await fetch('https://heroku-timeman.herokuapp.com/database/tables/', {
+        await fetch('http://localhost:8000/database/tables/', {
           method: 'post',
           headers: {
             'Content-Type': 'application/json'
@@ -94,7 +97,7 @@
       async editTable() {
         await this.getTables();
 
-        await fetch(`https://heroku-timeman.herokuapp.com/database/tables/${this.table.id}/`, {
+        await fetch(`http://localhost:8000/database/tables/${this.table.id}/`, {
           method: 'put',
           headers: {
             'Content-Type': 'application/json'
@@ -107,13 +110,75 @@
       async deleteTable(table) {
         await this.getTables();
 
-        await fetch(`https://heroku-timeman.herokuapp.com/database/tables/${table.id}/`, {
+        await fetch(`http://localhost:8000/database/tables/${table.id}/`, {
           method: 'delete',
           headers: {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify(this.table)
         });
+        await this.getTables();
+        this.table = {};
+      },
+
+
+      async getTablesAxios() {
+        var response = await axios({
+          url: 'http://localhost:8000/database/tables/',
+          method: 'get',
+        });
+        this.tables = await response.data;
+      },
+
+      
+      async createTableAxios() {
+        await this.getTables();
+
+        await axios({
+          url: 'http://localhost:8000/database/tables/',
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data: JSON.stringify(this.table)
+        });
+        await this.getTables();
+      },
+      async editTableAxios() {
+        await this.getTables();
+
+        await fetch({
+          url: `http://localhost:8000/database/tables/${this.table.id}/`,
+          method: 'put',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data: JSON.stringify(this.table)
+        });
+        await this.getTables();
+        this.table = {};
+      },
+
+      async deleteTableAxios(table) {
+        await this.getTables();
+
+/*
+        await axios.detele(`http://localhost:8000/database/tables/${table.id}/`, 
+          { headers: { 'Content-Type': 'application/json' } },
+          { data: { source: JSON.stringify(this.table) }});
+*/
+
+        //await axios.delete(`http://localhost:8000/database/tables/${table.id}/`, { data: JSON.stringify(this.table) });
+        
+        await axios({
+          url: `http://localhost:8000/database/tables/${table.id}/`,
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data: JSON.stringify(this.table)
+        });
+        
         await this.getTables();
         this.table = {};
       }
